83ca4881fa8481d97eafc816e3ad4e9dc1ea7a5c
Red-frame <135748645+Red-frame@users.noreply.github.com>
2025-02-18 14:03:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
-   const TelegramBot = require('node-telegram-bot-api');
+const { Telegraf } = require('telegraf');
 
-   const app = express();
-   const port = process.env.PORT || 3000;
+   const bot = new Telegraf(process.env.BOT_TOKEN);
 
-   // Ganti dengan token bot Anda
-   const token = '7908620487:AAF4g43C8WDQ_MPr2Eo9Dg2XYusyQbvMS6U';
-   const bot = new TelegramBot(token, { polling: true });
-
-   // Handle /start command
-   bot.onText(/\/start/, (msg) => {
-     const chatId = msg.chat.id;
-     bot.sendMessage(chatId, 'Hello! I am your Telegram bot.');
+   bot.start((ctx) => {
+     ctx.reply('Halo! Saya adalah bot Telegram.');
    });
 
-   // Handle /echo command
-   bot.onText(/\/echo (.+)/, (msg, match) => {
-     const chatId = msg.chat.id;
-     const resp = match[1]; // Pesan yang dikirim oleh pengguna
-     bot.sendMessage(chatId, `You said: ${resp}`);
+   bot.command('hello', (ctx) => {
+     ctx.reply('Halo juga!');
    });
 
-   // Start the server
-   app.listen(port, () => {
-     console.log(`Server is running on port ${port}`);
-   });
+   bot.launch();
 
-   module.exports = app;
+   // Export bot untuk Vercel
+   module.exports = bot;
